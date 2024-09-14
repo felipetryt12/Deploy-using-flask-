e4c7df149e9393b943e68575fc344f854cef5967
felipetryt12 <felipetryt12@gmail.com>
2024-09-14 16:46:43 -0600

    Deploy

--- src/app.py
+++ src/app.py
@@ -1,4 +1,24 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask, request, render_template
+import joblib
+import os
+import numpy as np
 
-# your code here
+app = Flask(__name__)
+
+modelo_path = os.path.join('models', 'modelo_regresion_lineal.pkl')
+modelo = joblib.load(modelo_path)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediccion = modelo.predict(final_features)
+
+    return render_template('index.html', prediction_text=f'La predicción de la calificación es: {prediccion[0]:.2f}')
+
+if __name__ == "__main__":
+    app.run(debug=True)
